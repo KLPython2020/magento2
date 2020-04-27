343bfc62604dc2d9c697eab5aeca6e6ba61d84f7
KLPython2020 <katherine.adjahoe@gmail.com>
2020-04-27 05:24:55 -0400

    added a new theme and logo

---
+++ app/design/frontend/Lawrence/lawrencetheme/registration.php
@@ -0,0 +1,8 @@
+<?php
+
+ use Magento\Framework\Component\ComponentRegistrar;
+ \Magento\Framework\Component\ComponentRegistrar::register(
+     ComponentRegistrar::THEME,
+    'frontend/Lawrence/lawrencetheme',
+    __DIR__
+);
